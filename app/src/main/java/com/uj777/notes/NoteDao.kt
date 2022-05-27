1617cfb28e71da4f0fa124ec0263202395793450
uj-git <umangjaiswal20@gmail.com>
2022-05-27 19:51:47 +0530

    Initial Commit

---
+++ app/src/main/java/com/uj777/notes/NoteDao.kt
@@ -0,0 +1,17 @@
+package com.uj777.notes
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NoteDao    {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: Note)
+
+    @Delete
+    suspend fun delete(note: Note)
+
+    @Query("SELECT * FROM notes_table ORDER BY id ASC" )
+    fun getAllNotes(): LiveData<List<Note>>
+}
